b62ebfd23525a7c4fc57615baa6e79edf9e0e5d8
BlueSgler <3032964348@qq.com>
2023-07-18 00:30:55 +0800

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+</template>
+
+<script setup lang='ts'>
+
+</script>
+
+<style lang='scss' scoped>
+
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>init</div>
+</template>
+
+<script setup lang='ts'>
+
+</script>
+
+<style lang='scss' scoped>
+
+</style>
