436f4ce01729b0af4da4f68c72490f9a5248d9e3
BlueSgler <3032964348@qq.com>
2023-07-18 01:13:37 +0800

    add proxy

--- vite.config.ts
+++ vite.config.ts
@@ -22,4 +22,16 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        // 配置需要代理的路径 --> 这里的意思是代理http://localhost:80/api/后的所有路由
+        target: 'http://xxxx.xxxx.xxxx.xxxx:xx', // 目标地址 --> 服务器地址
+        changeOrigin: true, // 允许跨域,
+        ws: true, // 允许websocket代理
+        // 重写路径 --> 作用与vue配置pathRewrite作用相同
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 });
