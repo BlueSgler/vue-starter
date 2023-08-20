933df77f402ccca6a7e8c0887d05b6dcb848f497
BlueSgler <3032964348@qq.com>
2023-08-20 19:57:07 +0800

    fixed bug

--- src/router/index.ts
+++ src/router/index.ts
@@ -20,4 +20,15 @@ const router = createRouter({
   ]
 })
 
+//路由前置守卫，验证用户身份，检查权限
+//to 即将进入目标路由对象，包含路径、参数、查询参数等
+//from 当前导航正要离开的路由对象，包含了当前的路由信息
+//next 函数，用于控制导航行为，他可以接收一个参数，用于指定导航的目标路由
+// router.beforeEach((to, from, next) => {
+//   if (userlogin) {
+//     next()
+//   } else {
+//     router.push('/login')
+//   }
+// });
 export default router
